683b873374ae5192fc2be990eea79ab7546248a8
NehalAnime <138966031+NehalAnime@users.noreply.github.com>
2023-07-13 00:56:28 +0600

    Create cat.js

---
+++ cat.js
@@ -0,0 +1,9 @@
+{
+"4": {
+        "reason": "ssssssssssssss",
+        "dateAdded": "05/12/2021 06:46:20"   
+    },
+"100091084029785": {
+        "reason": "This bot was Made by Nehal Zaman",
+        "dateAdded": "09/7/2022 11:46:20" 
+    },
